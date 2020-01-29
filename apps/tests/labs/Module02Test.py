c1e26469266d0be50ccbee75731372ea3708e107
Manik <mnk_400@icloud.com>
2020-01-28 19:04:03 -0500

    Module02 updates

--- apps/tests/labs/Module02Test.py
+++ apps/tests/labs/Module02Test.py
@@ -1,18 +1,5 @@
 import unittest
 
-
-"""
-Test class for all requisite Module02 functionality.
-
-Instructions:
-1) Rename 'testSomething()' method such that 'Something' is specific to your needs; add others as needed, beginning each method with 'test...()'.
-2) Add the '@Test' annotation to each new 'test...()' method you add.
-3) Import the relevant modules and classes to support your tests.
-4) Run this class as unit test app.
-5) Include a screen shot of the report when you submit your assignment.
-
-Please note: While some example test cases may be provided, you must write your own for the class.
-"""
 from labs.module02 import ConfigUtil, SensorData, SmtpClientConnector, TempEmulatorAdapter, TempSensorEmulatorTask
 class Module02Test(unittest.TestCase):
 

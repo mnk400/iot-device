680a42328f25c70cf8e143d72834e9748c767cc4
Manik <mnk_400@icloud.com>
2020-02-06 16:32:13 -0500

    Module03 init

--- apps/labs/module01/DeviceHandlerApp.py
+++ apps/labs/module01/DeviceHandlerApp.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     Creating and running a thread for Module01
     Set 'enableSystemPerformanceAdapter' to True to run
     '''
-    sysPerfAdaptor = SystemPerformanceAdapter.SystemPerformanceAdapter(2,12)  # 2,12 refers to sleep timer and loop counter
+    sysPerfAdaptor = SystemPerformanceAdapter.SystemPerformanceAdapter(2, 12)  #2,12 refers to sleep timer and loop counter
     sysPerfAdaptor.enableSystemPerformanceAdapter = False
     sysPerfAdaptor.daemon = True
     module01_Thread = threading.Thread(target=sysPerfAdaptor.run_adapter())
@@ -22,10 +22,9 @@ if __name__ == '__main__':
     Creating and running a thread for Module02
     Set 'enableTempEmulatorAdapter' to True to run
     '''
-    temperatureAdapter = TempEmulatorAdapter.TempEmulatorAdapter(2,12)
-    temperatureAdapter.enableTempEmulatorAdapter = True
+    temperatureAdapter = TempEmulatorAdapter.TempEmulatorAdapter(2 ,12)
+    temperatureAdapter.enableTempEmulatorAdapter = False
     temperatureAdapter.daemon = True
     module02_Thread = threading.Thread(target=temperatureAdapter.run_emulation())
 
-    
 

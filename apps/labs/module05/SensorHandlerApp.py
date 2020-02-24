a6ba7f0ed6331cacbf8534040423ce1d5ca3e69b
Manik <mnk_400@icloud.com>
2020-02-24 03:01:21 -0500

    Merge after this commit.

--- apps/labs/module05/SensorHandlerApp.py
+++ apps/labs/module05/SensorHandlerApp.py
@@ -8,9 +8,9 @@ from labs.module05 import MultiSensorAdapter
 
 if __name__ == '__main__':
     '''
-    Creating and running a thread for Module03
-    Set 'enableHumidityTask' to True to read from I2C
-    Set 'enableHI2CTask' to True to read from I2C
+    Creating and running a thread for Module05
+    Set 'enableTempTask' to run Temperature read thread
+    Set 'enableListener to run ActuatorData Listener thread
     Set both to run them both simultaneously
     '''
     module05                    = MultiSensorAdapter.MultiSensorAdapter(1,6)                     #Looptime and sleeptime of humidityTask and HI2CTask respectivley
